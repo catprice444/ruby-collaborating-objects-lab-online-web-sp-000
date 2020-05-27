ae6b3730a13af83960861685a33e138bda250484
Catherine Price <catprice444@gmail.com>
2020-05-27 17:29:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,9 +24,9 @@ class Song
 
   def self.new_by_filename(filename)
     song_name = filename.split(" - ")[1]
-    artist_name = filename.split(" - ")[0]
+    artist = filename.split(" - ")[0]
     song = Song.new(song_name)
-    song.artist_name = artist_name
+    song.artist_name = artist
     song
   end
 
